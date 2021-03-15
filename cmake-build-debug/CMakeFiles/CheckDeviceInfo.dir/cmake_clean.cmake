463666f0b277b796d27533763ed0d65d10eedb31
Seann Hicks <pageboy@hotmail.com>
2021-03-15 15:47:08 -0600

    Initial Revision

---
+++ cmake-build-debug/CMakeFiles/CheckDeviceInfo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CheckDeviceInfo.dir/cmake_device_link.o"
+  "CMakeFiles/CheckDeviceInfo.dir/main.cu.o"
+  "CheckDeviceInfo"
+  "CheckDeviceInfo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/CheckDeviceInfo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
